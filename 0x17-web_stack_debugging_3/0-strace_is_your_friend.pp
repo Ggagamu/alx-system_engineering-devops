7030e29fccacc08929412e959375337d911ffad6
Isaac <mugagga10@gmail.com>
2024-04-11 22:11:09 +0000

    0. Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fixes a wordpress site running on apache2
+$config_file = '/var/www/html/wp-settings.php'
+
+exec { 'edit_file':
+  command => "sed -i 's/phpp/php/g' ${config_file}",
+  path    => ['/bin','/usr/bin']
+}
